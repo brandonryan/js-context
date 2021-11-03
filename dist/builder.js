10c3f8803915b49d14508d6c44d3730dd3da9a5e
Brandon Ryan <bjryan19@protonmail.com>
2021-11-03 12:44:53 -0500

    added missing dist

---
+++ dist/builder.js
@@ -0,0 +1,86 @@
+/// <reference path="builder.d.ts" />
+//TODO: make it obvious that you are dealing with a builder if you try to use it like a context
+import { Context } from "./context.js";
+import { isPlainObject, isValidKey } from "./utils.js";
+//private variables
+const priv = new WeakMap();
+export class ContextBuilder {
+    constructor() {
+        priv.set(this, {});
+    }
+    with(key, value) {
+        const values = priv.get(this);
+        //in this case, treat key as values only arg
+        if (isPlainObject(key) && value === undefined) {
+            safegaurdKeys(this, key);
+            deepAssign(values, key);
+            return this;
+        }
+        if (!isValidKey(key)) {
+            throw new Error("Key must be a string or symbol");
+        }
+        safegaurdKey(this, key, value);
+        if (isPlainObject(value) && typeof key !== "symbol") {
+            deepAssign(values, { [key]: value });
+            return this;
+        }
+        values[key] = value;
+        return this;
+    }
+    withCtxFunction(name, fn) {
+        const values = priv.get(this);
+        safegaurdKey(this, name, fn);
+        //this cant be an arrow function
+        values[name] = function (...args) { return fn(this, ...args); };
+        return this;
+    }
+    asObject() {
+        throw new Error(`Can not convert ${ContextBuilder.name} to object`);
+    }
+    build(ctx) {
+        //if we get passed another ContextBuilder, we can make the destination inherit all the properties on this builder
+        if (ctx instanceof ContextBuilder) {
+            return ctx.with(priv.get(this));
+        }
+        if (!(ctx instanceof Context)) {
+            throw new Error("ctx must be an instance of Context or ContextBuilder");
+        }
+        //assign our values
+        const values = priv.get(this);
+        return ctx.with(values);
+    }
+}
+function deepAssign(dest, src) {
+    for (const key in src) {
+        if (isPlainObject(src[key])) {
+            if (dest[key] === undefined) {
+                dest[key] = {};
+            }
+            deepAssign(dest[key], src[key]);
+        }
+        else {
+            dest[key] = src[key];
+        }
+    }
+    //we need to manually assign any symbols
+    for (const sym of Object.getOwnPropertySymbols(src)) {
+        dest[sym] = src[sym];
+    }
+}
+function safegaurdKeys(builder, obj) {
+    for (const key in obj) {
+        safegaurdKey(builder, key, obj[key]);
+    }
+}
+function safegaurdKey(builder, key, value) {
+    //we cant set the property if it is already defined
+    if (builder.hasOwnProperty(key)) {
+        return;
+    }
+    Object.defineProperty(builder, key, {
+        enumerable: typeof value === "function" ? false : true,
+        get: () => {
+            throw new Error("ContextBuilder does not allow retrieving properties. build() the context first.");
+        }
+    });
+}
---
+++ dist/modules/cancel.js
@@ -0,0 +1,91 @@
+import { ContextBuilder } from "../index";
+const cancelSym = Symbol("cancel-state");
+/**
+ * Sets cancellation on the context
+ * use cancelCtx to cancel the context
+ * use ctx.whenCancelled or whenCancelled to listen for ctx cancellation
+ * TODO: notes about context cancellation with parents/children
+ */
+export function withCancel(ctx) {
+    const ctxState = {
+        reason: undefined,
+        callbacks: [],
+        timeoutId: 0,
+    };
+    if (hasCancellation(ctx)) {
+        ctxState.parent = ctx[cancelSym];
+    }
+    //set up the functional bit
+    return new ContextBuilder()
+        .with(cancelSym, ctxState)
+        .withCtxFunction("whenCancelled", whenCancelled)
+        .build(ctx);
+}
+export function cancelCtx(ctx, reason) {
+    const state = getCancelState(ctx);
+    if (typeof reason !== 'string')
+        throw new Error("Must provide a reason for cancellation.");
+    if (getCancelReason(state) !== undefined)
+        return;
+    state.reason = reason;
+    for (const callb of state.callbacks) {
+        callb(state.reason);
+    }
+}
+export async function whenCancelled(ctx) {
+    const state = getCancelState(ctx);
+    //if its already cancelled, we throw early because we wont get a callback
+    const cancel = getCancelReason(state);
+    if (cancel) {
+        throw new Error(fmtReason(cancel));
+    }
+    //return a promise that will be rejected when callbacks are called
+    return await new Promise((res, rej) => {
+        addCallbackToContext(state, (reason) => {
+            rej(new Error(fmtReason(reason)));
+        });
+    });
+}
+/**
+ * Adds cancellation to context that will be cancelled when specified timeout is reached
+ */
+export function withCancelTimeout(ctx, ms) {
+    const child = withCancel(ctx);
+    const state = getCancelState(child);
+    //Nodejs has weird typings. Have to convert to number with +
+    state.timeoutId = +setTimeout(cancelCtx, ms, child, "timeout");
+    return child;
+}
+export function cancelTimeout(ctx) {
+    const state = getCancelState(ctx);
+    clearTimeout(state.timeoutId);
+}
+function hasCancellation(ctx) {
+    return ctx[cancelSym] !== undefined;
+}
+//checks recursively to find if the ctx is cancelled
+//returns reason if it is, undefined otherwise
+function getCancelReason(state) {
+    if (state.reason !== undefined)
+        return state.reason;
+    if (state.parent)
+        return getCancelReason(state.parent);
+    return;
+}
+//adds the callback recursively to any parent cancellation context
+function addCallbackToContext(state, callb) {
+    state.callbacks.push(callb);
+    if (state.parent !== undefined) {
+        addCallbackToContext(state.parent, callb);
+    }
+}
+function fmtReason(reason) {
+    return `Context has been cancelled. Reason: ${reason}`;
+}
+function getCancelState(ctx) {
+    const state = ctx[cancelSym];
+    if (state === undefined) {
+        throw new Error("Context does not have cancellation set.");
+    }
+    return state;
+}
